d5716c3494b12f10cf6a1254c0dec1d0ed497bae
canerilme <canerilme@e-yaz.com.tr>
2023-09-27 00:09:46 +0300

    OperationClaimSeed was added in AuthServer

---
+++ src/projects/AuthServer/AuthServer.API/Persistance/Seed/OperationClaimSeed.cs
@@ -0,0 +1,17 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthServer.API.Persistance.Seed
+{
+    public class OperationClaimSeed : IEntityTypeConfiguration<OperationClaim>
+    {
+        public void Configure(EntityTypeBuilder<OperationClaim> builder)
+        {
+            builder.HasData(
+                new OperationClaim { Id=1,Name="admin"},
+                new OperationClaim { Id = 2, Name = "moderator" },
+                new OperationClaim { Id = 3, Name = "user" });
+        }
+    }
+}
